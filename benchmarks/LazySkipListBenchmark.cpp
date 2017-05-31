ad8b49c5f7d1d193ac82c660bf60646c66a21b92
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2017-05-31 13:39:38 +0200

    Add a benchmark for lazy skip list

---
+++ benchmarks/LazySkipListBenchmark.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+
+#include "Benchmarking.h"
+#include "LazySkipList.h"
+#include "WorkStrategy.h"
+
+int main(int argc, char** argv)
+{
+    std::vector<BenchmarkConfiguration> benchmarks;
+
+    BenchmarkConfiguration benchmarkTemplate;
+    benchmarkTemplate.numberOfThreads = 8;
+    benchmarkTemplate.numberOfItems = 250000;
+    benchmarkTemplate.initialNumberOfItems = 10000;
+    benchmarkTemplate.listFactory = [] {
+        return std::make_unique<LazySkipList<int, 16>>();
+    };
+
+    {
+        BenchmarkConfiguration benchmark = benchmarkTemplate;
+        benchmark.description = "ascending insert";
+        benchmark.workStrategy = WorkStrategy::AscendingInsert;
+        benchmarks.push_back(benchmark);
+    }
+
+    {
+        BenchmarkConfiguration benchmark = benchmarkTemplate;
+        benchmark.description = "descending insert";
+        benchmark.workStrategy = WorkStrategy::DescendingInsert;
+        benchmarks.push_back(benchmark);
+    }
+
+    {
+        BenchmarkConfiguration benchmark = benchmarkTemplate;
+        benchmark.description = "interleaving insert";
+        benchmark.workStrategy = WorkStrategy::InterleavingInsert;
+        benchmarks.push_back(benchmark);
+    }
+
+    const auto results = runBenchmarks(benchmarks);
+    saveBenchmarkResultsAsCsv(benchmarks, results, "LazySkipList");
+
+    return EXIT_SUCCESS;
+}
