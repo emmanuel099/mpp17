9793d22192ef2d85286d7adff01cb1e9e335b65e
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2017-06-01 12:16:01 +0200

    Remove explicit next freeing in node dtor

--- src/LazySkipList.h
+++ src/LazySkipList.h
@@ -32,13 +32,6 @@ class LazySkipList final : public SkipList<T>
         {
         }
 
-        ~Node()
-        {
-            for (std::uint16_t i = 0; i < MaximumHeight; ++i) {
-                next[i].reset();
-            }
-        }
-
         const value_type value;
         std::array<std::shared_ptr<Node>, MaximumHeight> next;
         const std::uint16_t height;
